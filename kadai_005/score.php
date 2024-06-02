8925ccab4af4ac64e8c5ae0dee1d6f98334f7b54
pikariru <167439420+pikariru@users.noreply.github.com>
2024-06-02 19:42:18 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $score_total = 80+60+55+40+100+25 + 95 +30 +60 ;
+         $score_average = $score_total/10;
+         echo $score_average;
+         ?>
+      </p>
+ </body>
+ 
+ </html>
